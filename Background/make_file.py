1e13e74caca0f9ae7745c8acd75d09267ff0902f
Moomin03 <janghongseo@naver.com>
2024-09-05 15:22:06 +0900

    This is my first project

--- Background/make_file.py
+++ Background/make_file.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
-a = pd.read_csv('../csv/FRAME.csv')
-b = pd.read_csv('home_data4.csv')
+a = pd.read_csv('/Users/hack/PycharmProjects/Project_1/csv/FRAME.csv')
+b = pd.read_csv('/Users/hack/PycharmProjects/Project_1/home_data5.csv')
 new_file = pd.concat([a, b], axis=0)
 new_file.to_csv('FRAME.csv')
